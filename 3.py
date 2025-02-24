2de3c7c1b76f40510f0f6e675dd614e5ad9b08a7
Akshay D <bl.en.u4aie23072@bl.students.amrita.edu>
2025-02-24 20:33:18 +0530

    3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,8 @@
+# User input 
 list1 = input("Enter the elements of list 1 : ")
 list2 = input("Enter the elements of list 2 : ")
+
+# Function to find common element
 def find_common_elements(list1, list2):
     set_list1=set(list1)
     set_list2=set(list2)
