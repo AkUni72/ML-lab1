f574bb7c07fb589f54e2d442e9f654c974a6028a
Akshay D <bl.en.u4aie23072@bl.students.amrita.edu>
2025-02-24 19:41:45 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 str = input("Enter the string : ")
-vovels = 0
-conconents = 0
+vovels = 0 # Initialize vowel count
+conconents = 0 # Initialize conconent count
 
 for char in str:
-    if char in ("a","e","i","o","u"):
-        vovels = vovels + 1
-    elif char.isalpha():
-        conconents = conconents + 1
+    if char in ("a","e","i","o","u"): # Check if it's a vowel
+        vovels = vovels + 1 # Increment vovel by 1
+    elif char.isalpha(): # Check if it's a conconent
+        conconents = conconents + 1 # Increment conconent by 1
 
 print("Number of vowels are : ", vovels)
 print("Number of consonents are : ", conconents)
