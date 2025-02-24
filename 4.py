32677e09706c9ed541e94ffa2200fa9eb6d364d5
Akshay D <bl.en.u4aie23072@bl.students.amrita.edu>
2025-02-24 21:36:54 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,8 +1,11 @@
+# User input
 matrix=input("Enter the matrix: ")
+
+# Function to transpose a matrix
 def matrix_transpose(mat):
-    M=[]
+    M=[] # Initialize an empty list to store the transposed matrix
     for i in range(len(mat)):
-        temp=[]
+        temp=[]  # Temporary list to store each row of the transposed matrix
         for j in range(len(mat[0])):
             temp.append(mat[j][i])
         M.append(temp)
